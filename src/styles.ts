e19133df611d12d5f963307f8200aff8dac94a38
Chris W <chriswxyz@outlook.com>
2016-11-04 14:26:36 -0500

    Pull in strongback css and react

---
+++ src/styles.ts
@@ -0,0 +1,6 @@
+import '@redarrowlabs/strongback-css';
+import 'sweetalert2/dist/sweetalert2.css';
+
+//Our styles go last, as overrides.
+import './styles/styles.css';
+import './styles/sass-styles.scss';
